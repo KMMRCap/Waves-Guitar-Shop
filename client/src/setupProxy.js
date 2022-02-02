2804d7347a950724c580495a7e7dd288cff06be5
KMMRCap <mortezarezainia@gmail.com>
2022-02-02 21:13:41 +0000

    temp change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3002',
+      target: 'https://waves-the-guitar-shop.herokuapp.com',
       changeOrigin: true,
     })
   );
